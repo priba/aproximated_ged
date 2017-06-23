7586f564339c2e464a290e8ba238aacadd3c0ede
Pau Riba <priba@cvc.uab.cat>
2017-06-23 18:04:00 +0200

    evaluate folder structure

--- evaluate.py
+++ evaluate.py
@@ -21,4 +21,27 @@ import argparse
 __author__ = "Pau Riba, Anjan Dutta"
 __email__ = "priba@cvc.uab.cat, adutta@cvc.uab.cat"
 
+# Argument parser
+parser = argparse.ArgumentParser(description='Computes the distance all vs all between two folders.')
+
+# Prototypes
+parser.add_argument('--folder1', help='Input graphs', default='./data/Letters/test/')
+parser.add_argument('--folder2', help='Graphs agains whose computes the distance', default='./data/Letters/train/')
+
+args = parser.parse_args()
+
+
+def main(fold1, fold2, ged):
+    # TODO
+    for f1, f2 in itertools.combinations_with_replacement(files, 2):
+        # Read graphs
+        g1 = nx.read_gml(f1)
+        g2 = nx.read_gml(f2)
+
+        # Distance
+        dist, assignment = ged.ged(g1, g2)
+    pass
+
 if __name__ == '__main__':
+    ged = VanillaHED()
+    main(args.folder1, args.folder2, ged)
